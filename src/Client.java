0653cf38c7c88084cdf5380992a7471f4e5ad547
Wonkyo Choe <heysid@ajou.ac.kr>
2019-05-28 11:30:58 +0900

    WIP: reconnection mechanism when the capacitiy is full

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,21 @@
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Client {
     private Socket socket;
 
+    public void checkReconn() {
+
+    }
+
     public Client(String host, int port, String file) {
         try {
             socket = new Socket(host, port);
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -17,6 +24,6 @@ public class Client {
     }
 
     public static void main(String[] args) {
-        Client c = new Client("localhost", 1933, "asdad.jpg");
+        Client c = new Client("localhost", 7000, args[2]);
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,8 @@
 // ref: https://gist.github.com/CarlEkerot/2693246
 import java.io.IOException;
 import java.lang.ClassNotFoundException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +10,16 @@ public class Server extends Thread {
     private ServerSocket server;
     private int port;
     private int numOfClient = 0;
+    private final int LIMIT = 4;
+
+    public Server(int port) {
+        this.port = port;
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int getNumOfClient() {
         return numOfClient;
@@ -17,19 +29,41 @@ public class Server extends Thread {
         this.numOfClient = numOfClient;
     }
 
+    private boolean IsFull() {
+        if (this.numOfClient > this.LIMIT) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void sendMsg(Socket client, String[] msgArr) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+        oos.writeObject(msgArr);
+    }
+
     public void run() {
        while (true) {
            try {
                Socket client = server.accept();
+               if (IsFull()) {
+                   String[] msgArr = new String[]{"full", "7001"};
+                   sendMsg(client, msgArr);
+               }
+               setNumOfClient(getNumOfClient() + 1);
+               // transfer();
+
            } catch (IOException e) {
                e.printStackTrace();
            }
        }
     }
 
-    public void main(String args[]) throws IOException, ClassNotFoundException {
+    public void main(String[] args) throws IOException, ClassNotFoundException {
         port = Integer.valueOf(args[0]);
-        server = new ServerSocket(port);
+        Server s = new Server(port);
+        s.start();
+
         System.out.println("Shutting down socket server!!");
         server.close();
     }
